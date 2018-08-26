dfd66475b6461565eb796054ca7e9184b058e4d8
Savras <d20.savras@gmail.com>
2018-08-26 01:00:59 -0700

    Added preliminary model

--- imdb.py
+++ imdb.py
@@ -1,6 +1,5 @@
 #from keras.datasets import imdb
 import tensorflow as tf
-import numpy as np
 
 (train_data, train_labels), (test_data, test_labels) = tf.keras.datasets.imdb.load_data(num_words=10000)
 print(train_data[0])
@@ -14,6 +13,7 @@ decoded_review = ' '.join([reverse_word_index.get(i - 4, '?') for i in train_dat
 print(decoded_review)
 
 # vectorize
+import numpy as np
 def vectorize_sequence(sequences, dimensions=10000):
     results = np.zeros((len(sequences), dimensions))
     for i, sequence in enumerate (sequences):
@@ -28,3 +28,23 @@ print(x_text[0])
 
 y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
+
+# model
+from keras import models
+from keras import layers
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy' , metrics=['accuracy'])
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
+history_dict = history.history
+print(history_dict.keys())
