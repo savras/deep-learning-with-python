10d227b9125f72b2f9f707ce78988b04c619646c
Savras <d20.savras@gmail.com>
2018-09-11 00:08:33 -0700

    Fixed pyplot and epochs

--- imdb.py
+++ imdb.py
@@ -48,12 +48,14 @@ history_dict = history.history
 print(history_dict.keys())
 
 # draw matlab graph
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 loss_value = history_dict['loss']
 val_loss_values = history_dict['val_loss']
 
-epochs = range(1, len(acc) + 1)
+epochs = range(1, len(history_dict['acc']) + 1)
 
 plt.plot(epochs, loss_value, 'bo', label='Training loss')
 plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
