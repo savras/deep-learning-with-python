827b08cd6d50ec49775a94c271acb98340ca75a9
Vincent Gim <d20.savras@gmail.com>
2018-10-06 00:34:57 -0700

    Fixed np.zeros function

--- reuters.py
+++ reuters.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import np as numpy
 #from tf.keras.datasets import reuters
 
 
@@ -10,11 +9,11 @@ reverse_word_index = dict ([(value, key) for (key, value) in word_index.items()]
 decoded_newswire = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
 
 print(decoded_newswire)
-
 print(train_labels[10])
 
+import numpy as np
 def vectorize_sequences(sequences, dimension=10000):
-    results = np.zeros(len(sequences))
+    results = np.zeros((len(sequences), dimension))
     for i, sequence in enumerate(sequences):
         results[i, sequence] = 1.
     return results
@@ -22,8 +21,9 @@ def vectorize_sequences(sequences, dimension=10000):
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
 
+# Built-in to_categorical(train_data)
 def to_one_hot(labels, dimension=46):
-    results = np.zeros(len(labels), dimension)
+    results = np.zeros((len(labels), dimension))
     for i, label in enumerate(labels):
         results[i, label] = 1.
     return results
